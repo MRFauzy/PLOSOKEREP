c7095e806d73b860d679e161009cbb829883eeba
MRFauzy <fauzyrandi5@gmail.com>
2024-12-09 18:08:35 +0700

    first commit

--- resources/views/layout/copyright.blade.php
+++ resources/views/layout/copyright.blade.php
@@ -1,3 +1,3 @@
 <footer class="pb-2">
-    <p class="text-center">&copy; 2024 kolompok 1. All rights reserved.</p>
+    <p class="text-center">&copy; 2024 kolompok 5. All rights reserved.</p>
 </footer>
